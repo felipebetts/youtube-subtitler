33f78b647bb9427334f273553d0dc2767e14e5b4
felipebetts <felipe.a.betts@gmail.com>
2023-04-11 23:34:57 -0300

    feat: transcribe api route

---
+++ pages/api/transcribe.tsx
@@ -0,0 +1,23 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { spawn } from 'child_process'
+import path from 'path'
+import { transferChildProcessOutput } from '../../utils/shell'
+
+export default function GET(
+    request: NextApiRequest,
+    response: NextApiResponse
+) {
+    const video_id = request.query.video_id as string
+    if (typeof video_id !== 'string') {
+        response.status(400).json({ error: 'Invalid input' })
+        return
+    }
+
+    console.log('video_id:', video_id)
+
+    const cmd = spawn('python3', [
+        path.join(process.cwd(), 'scripts/transcribe.py'),
+        video_id || ''
+    ])
+    transferChildProcessOutput(cmd, response)
+}
